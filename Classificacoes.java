094a24f7077c088778aebca16bac30fee761f068
JuJubali <92491433+JuJubali@users.noreply.github.com>
2023-02-16 02:00:26 -0300

    pato

--- Classificacoes.java
+++ Classificacoes.java
@@ -1,1 +1,7 @@
 // Classificação de cada pato
+public class Classificacoes {
+    Pato pato1 = new Pato("ciencia", "Médico");
+    Pato pato2 = new Pato("ciencia", "Hack");
+    Pato pato3 = new Pato("ciencia", "Físico");
+    
+}
---
+++ Pato.java
@@ -0,0 +1,11 @@
+
+public class Pato {
+    public String classe;
+    public String profissao;
+
+    public Pato () {}
+    public Pato (String classe, String profissao) { 
+        this.classe = classe;
+        this.profissao = profissao;
+    }
+}
